d4f146f1be1037b27bf32041401daa647478eb29
Aulijah-Waugh <Aulijahgreen@gmail.com>
2024-09-18 21:05:31 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -12,7 +12,42 @@ public class Assignment1 {
     System.out.println(" AAAAAAAAAAAAA    WW   WWW   WW  ");
     System.out.println(" AAAA     AAAA      WWW   WWW    "); 
     System.out.println(" AAAA     AAAA       WW   WW     ");  
+    System.out.println("                                 ");
 
+  
+   
+      //Converting Fahrenheit to Celsius
+    
+      
+      //Setting sc As A Private Static
+      Scanner scan; {
+        scan= new Scanner(System.in);
+      //Setting Output And Receiving Input
+        System.out.println(" Type in a Temperature in Farenheit:  ");
+        double Farenheit = scan.nextDouble(); 
 
+      //Math For Conversion
+        double Celsius = (Farenheit - 32) * 5 / 9;
+      //Outputing Celsius
+        System.out.println("The Farenheit You Gave Was: " + Farenheit    );
+        System.out.println( "Your Temperature In Celsius Is: " + Celsius );
+        System.out.println("                                          ");
+      
+          //Generate Random Number 
+        class Randomnum {
+          //Setting Minimum and Maximum
+          int min = 32 - 16;
+
+          int max = 384;
+
+          Random random = new Random();
+          
+          //Setting up Random Number Generation
+          int randomnumber = random.nextInt(max - min + 1) + min; 
+
+          //Outputing Random Number
+          
+        }
+     }
   }
 }
