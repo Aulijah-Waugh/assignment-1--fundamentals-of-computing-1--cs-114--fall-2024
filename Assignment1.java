df9f4be84ac6f7457610afb7642f3b2087dba87f
Aulijah-Waugh <Aulijahgreen@gmail.com>
2024-09-18 20:00:51 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,18 @@
 import java.util.Scanner; 
+import java.util.Random;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    // Using println to print out my initial 
+
+    System.out.println("     AAAAA       WW     W     WW ");
+    System.out.println("    AAAAAAA      WWW   WWW   WWW ");
+    System.out.println("   AA     AA     WWW   WWW   WWW ");
+    System.out.println("  AAA     AAA    WWW   WWW   WWW ");
+    System.out.println(" AAAAAAAAAAAAA    WW   WWW   WW  ");
+    System.out.println(" AAAA     AAAA      WWW   WWW    "); 
+    System.out.println(" AAAA     AAAA       WW   WW     ");  
+
+
   }
 }
